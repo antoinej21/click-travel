d00d8bf26e062e84fad9135d5ef63ebf33d7734f
Antoine <antoine.joly@diiage.org>
2020-11-19 11:29:26 +0100

    init

---
+++ components/Logo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="logo"></div>
+</template>
+
+<style>
+.logo {
+  animation: 1s appear;
+  margin: auto;
+  background-image: url('~assets/logo.svg');
+  background-size: contain;
+  background-repeat: no-repeat;
+  height: 60px;
+  margin: 10px 12px;
+}
+
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="container">
+    <div class="toolbar">
+      <Logo />
+    </div>
+    <div class="content">
+      <div>
+        <h1 class="title">Choose your dream destination...</h1>
+        <div class="links">
+          <a href="#" class="giant-button"> San Francisco, USA </a>
+          <a href="#" class="giant-button"> Québec, Canada </a>
+          <a href="#" class="giant-button"> Bora Bora, Polynésie </a>
+          <a href="#" class="giant-button"> Torres del Paine, Chile </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+
+.toolbar {
+  height: 80px;
+  background-color: #35495e;
+  align-self: flex-start;
+  width: 100%;
+}
+
+.content {
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  background-image: url('~assets/airplane.jpg');
+  background-size: cover;
+}
+
+.title {
+  font-weight: 500;
+  font-size: 58px;
+  color: #fff;
+  letter-spacing: 1px;
+  text-shadow: 1px 1px 4px #000;
+  text-align: center;
+}
+
+.links {
+  display: flex;
+  flex-flow: row wrap;
+  align-items: center;
+  justify-content: center;
+  padding-top: 15px;
+  & > * {
+    margin: 5px;
+  }
+}
+</style>
